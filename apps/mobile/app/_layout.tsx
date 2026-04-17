de463a62867555056d5083ab9dcfd9fb724ead05
FranF3 <franco.amoroso@fathom3dev.com>
2026-04-17 13:33:14 +0200

    added expo navigation bar y expo haptics

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -2,10 +2,18 @@ import "../global.css";
 import { useFonts as useInterFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from "@expo-google-fonts/inter";
 import { useFonts as useSpaceGroteskFonts, SpaceGrotesk_500Medium, SpaceGrotesk_600SemiBold, SpaceGrotesk_700Bold } from "@expo-google-fonts/space-grotesk";
 import { PortalHost } from "@rn-primitives/portal";
+// import * as NavigationBar from 'expo-navigation-bar';
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+// import { Platform } from "react-native";
+
+// const isAndroid = Platform.OS === 'android';
+// if (isAndroid) {
+//   NavigationBar.setBackgroundColorAsync('black')
+//   NavigationBar.setButtonStyleAsync('light')
+// }
 
 SplashScreen.preventAutoHideAsync();
 
